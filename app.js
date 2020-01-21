7a0fa5fb2885d4528f40ceb7a80b92f9f08c99e1
Harrison Thomas <hthomas146@utexas.edu>
2020-01-20 19:25:47 -0600

    Working on prompts for creating employee templates

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-var inquirer = require("inquirer");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+let Employee = require("Develop/lib/Employee.js");
+let Manager = require("Develop/lib/Manager.js");
+let Intern = require("Develop/lib/Intern.js");
+let Engineer = require("Develop/lib/Engineer.js");
+
+// var main = require("Develop/templates/main.html");
+// var engineerHTML = require("Develop/templates/engineer.html");
+// var managerHTML = require("Develop/templates/manager.html");
+// var internHTML = require("Develop/templates/intern.html");
+
 
 inquirer
     .prompt([{
@@ -27,9 +39,26 @@ inquirer
         choices: [
             "Manager",
             "Engineer",
-            "Intern"
-        ]
+            "Intern",
+        ],
+        name: "role"
+    }, {
+        type: "input",
+        name: "school",
+        message: "What school are you attending?",
+        when: (response) => response.role === 'Intern'
+    }, {
+        type: "input",
+        name: "github",
+        message: "What is your github url?",
+        when: (response) => response.role === 'Engineer'
+    }, {
+        type: "input",
+        name: "officeNo",
+        message: "What is your office number?",
+        when: (response) => response.role === "Manager"
     }])
-    .then(function (response) {
+    .then(
+        function (response) {
 
-    })
+        })
